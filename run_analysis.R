95467a6437d860903400b5fe10315ebc8cd728af
deng <deng1919@gmail.com>
2014-04-23 17:29:14 -0700

    modification to change colnames

--- run_analysis.R
+++ run_analysis.R
@@ -13,7 +13,7 @@ train_all<-cbind(train_sub, train_activitylabel, train_df)
 all_df<-rbind(train_all, test_all)
 
 feature<-read.table('./UCI HAR Dataset/features.txt')
-col_name_vector<-c('Subject','Activity_Label',as.character(feature[[2]]))
+col_name_vector<-c('Subject','ActivityLabel', as.character(feature[[2]]))
 colnames(all_df)<-col_name_vector
 
 # part 2: select columns containing only mean and std of each measurement
@@ -21,41 +21,39 @@ colnames(all_df)<-col_name_vector
 mean_col<-grepl('-mean()', col_name_vector, fixed=TRUE)
 std_col<-grepl('-std()', col_name_vector, fixed=TRUE)
 sub_col<-grepl('Subject', col_name_vector)
-al_col<-grepl('Activity_Label', col_name_vector)
+al_col<-grepl('ActivityLabel', col_name_vector)
 col_select<-mean_col|std_col|sub_col|al_col
 
 df_meanstd<-all_df[,col_select]
 
 # part 3/4: put descriptive activity label in the data frames (for both the 
 # complete data set and the data set contain only mean and std)
+
 activity_labels<-read.table('./UCI HAR Dataset/activity_labels.txt')
 actlab_vect<-as.character(activity_labels[[2]])
 for (i in 1:nrow(activity_labels)){
-    index<-which(df_meanstd$Activity_Label==activity_labels[i,1])
-    df_meanstd[index,]$Activity_Label<-actlab_vect[i]
-    all_df[index,]$Activity_Label<-actlab_vect[i]
+    index<-which(df_meanstd$ActivityLabel==activity_labels[i,1])
+    df_meanstd[index,]$ActivityLabel<-actlab_vect[i]
 }
 
 # part 5: calculating the average of each variable in the complete data set
 # for each subject and each activity
-f1<-as.factor(all_df$Subject)
-f2<-ordered(all_df$Activity_Label, levels=as.character(activity_labels$V2))
 
-s_df<-split(all_df,list(f1,f2))
-n_col<-ncol(all_df)
-sum<-sapply(s_df, function(y) colMeans(y[,3:n_col]))
+f1<-as.factor(df_meanstd$Subject)
+f2<-ordered(df_meanstd$ActivityLabel, levels=as.character(activity_labels$V2))
 
 s_df_meanstd<-split(df_meanstd, list(f1, f2))
 n_col_meanstd<-ncol(df_meanstd)
 sum_meanstd<-sapply(s_df_meanstd, function(y) colMeans(y[,3:n_col_meanstd]))
 
-sub<-data.frame(Subject=rep(1:30, times=6))
-act<-data.frame(Activity=rep(actlab_vect, each=30))
-
-tp_sum<-as.data.frame(t(sum))
-output<-cbind(sub, act, tp_sum, row.names=NULL)
-write.table(output, './tidydata.txt', sep='\t')
-
+#sub<-data.frame(Subject=rep(1:30, times=6))
+#act<-data.frame(Activity=rep(actlab_vect, each=30))
 tp_sum_meanstd<-as.data.frame(t(sum_meanstd))
-output_meanstd<-cbind(sub, act, tp_sum_meanstd, row.names=NULL)
+rname<-strsplit(rownames(tp_sum_meanstd), '\\.')
+sub<-function(x){x[1]}
+act<-function(x){x[2]}
+Subject<-sapply(rname, sub)
+Activity<-sapply(rname, act)
+output_meanstd<-cbind(Subject, Activity, tp_sum_meanstd, row.names=NULL)
+colnames(output_meanstd)<-gsub('\\()','',gsub('-','',colnames(output_meanstd)))
 write.table(output_meanstd, './tidydata_meanstd.txt', row.names=F)
